aed73d325512e5a6f29282a7207fa7295fdb9ee1
Carolina Campos <carolinacampos.dev@gmail.com>
2026-09-24 17:51:11 -0300

    Mostra galpoes e detalhes a partir de uma lista de dados

---
+++ dados-galpoes.js
@@ -0,0 +1,11 @@
+const galpoes = [
+  {
+    id: "exemplo",
+    nome: "Galpão exemplo",
+    cidade: "Cidade a informar",
+    uf: "UF",
+    area: "Área a informar",
+    finalidade: "Locação ou venda",
+    descricao: "Substituiremos este texto pelos dados reais do imóvel."
+  }
+];
